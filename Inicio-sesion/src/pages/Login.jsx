43ff4df3033ddfe030aaaa07099b21cea28bcaad
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-09-14 09:44:38 -0500

    feat: :sparkles: Login
    Se crea la funcion que va a contener la funcion de inicio de sesion
    
    by: Jhon Hernandez

---
+++ Inicio-sesion/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import InicioSesion from '../components/formulary/formulary'
+
+export default function Login() {
+    return (
+        <>
+            <InicioSesion />
+        </>
+    )
+}
