f924ba1bfab49dc74849aefe824752c7c0b7b7af
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-09-14 09:39:41 -0500

    feat: :sparkles: App.jsx
    Se crea el archivo app.jsx y se crea las rutas de las pages
    
    by: Jhon Hernandez

---
+++ Inicio-sesion/src/routes/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from '../pages/Login'
+import Inicio from '../pages/Inicio'
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element = {<Login />}/>
+                <Route path='/inicio' element = {<Inicio />}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
